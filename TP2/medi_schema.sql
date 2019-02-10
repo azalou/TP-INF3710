3c3db5c00a82ee1e3827e62da617f8bed3bd7f9c
Alassane Maiga <azou8506@gmail.com>
2019-02-10 17:16:30 -0500

    TPs first commit

---
+++ TP2/medi_schema.sql
@@ -0,0 +1,64 @@
+SET search_path = medi;
+
+DROP SCHEMA IF EXISTS mediDB CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS mediDB;
+
+CREATE TABLE IF NOT EXISTS mediDB.patient (
+	pID			Varchar(10)		Not NULL,
+	pName		Varchar(20)		Not NULL,
+	pNAS        VARCHAR(15)     UNIQUE Not NULL,
+	pAddress	Varchar(100)	Not NULL,
+	pPhone		Varchar(50)		Not NULL,
+	pDOB		Date			Not NULL,
+	PRIMARY KEY (pID)
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.doctor(
+	dID			Varchar(10)		UNIQUE Not NULL,
+	dName		Varchar(20)		Not NULL,
+	dAddress	Varchar(100)	Not NULL,
+	dPhone		Varchar(50)		Not NULL,
+	dDOB		Date			Not NULL,
+	dSalary		Numeric(8,2)	Not Null,
+	PRIMARY KEY (dID),
+	CONSTRAINT onSalary CHECK (dSalary>=100000.00)
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.medicald(
+	dID			Varchar(10)		Not NULL,
+	dOtimeRate	Numeric(6,2)	Not NULL,
+	PRIMARY KEY (dID),
+	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.specialistd(
+	dID			Varchar(10)		Not NULL,
+	dFieldArea	Varchar(30)		Not NULL,
+	PRIMARY KEY (dID),
+	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.bill(
+	bID			Varchar(10)		UNIQUE Not NULL,
+	dID			Varchar(10)		Not NULL,
+	PRIMARY KEY (bID, dID),
+	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.payment(
+	payID		Varchar(10)		UNIQUE Not NULL,
+	pID			Varchar(10)		Not NULL,
+	pDetails	Varchar(100)	Not NULL,
+	pMethod		Varchar(100)	Not NULL,
+	PRIMARY KEY (payID, pID),
+	FOREIGN KEY (pID) REFERENCES mediDB.patient(pID) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS mediDB.billpayments(
+	payID		Varchar(10)		Not NULL,
+	bID			Varchar(10)		Not NULL,-- pas sur ici
+	PRIMARY KEY (payID, bID),
+	FOREIGN KEY (payID) REFERENCES mediDB.payment(payID) ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (bID) REFERENCES mediDB.bill(bID) ON DELETE RESTRICT ON UPDATE CASCADE
+);
