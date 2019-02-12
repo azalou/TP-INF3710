7a61c17a2fef4f8fd20dd61ea51a2ca37a412691
Alassane Maiga <azou8506@gmail.com>
2019-02-12 17:55:35 -0500

    small changes

--- TP2/medi_query.sql
+++ TP2/medi_query.sql
@@ -1,24 +1,24 @@
 /*a) Affichez tous les docteurs et leurs détails.*/
 SELECT *
-FROM medi_schema.doctor;
+FROM mediDB.doctor;
 
 /*b) Affichez les patients et leurs paiements.*/
 SELECT * 
-FROM medi_schema.patient
-LEFT JOIN medi_schema.payment USING (pID);
+FROM mediDB.patient
+LEFT JOIN mediDB.payment USING (pID);
 
 /*c) Quels sont les rendez-vous (Appointment) du docteur dont le matricule est D001 ?*/
 SELECT *
-FROM medi_schema.appointment
+FROM mediDB.appointment
 WHERE (dId='D001');
 
 /*d) Listez tous les spécialistes, incluant tous leurs attributs de médecin et leur champ de
 spécialité (FieldArea).*/
 SELECT * 
-FROM medi_schema.specialist
-LEFT JOIN medi_schema.doctor USING (dId);
+FROM mediDB.specialist
+LEFT JOIN mediDB.doctor USING (dId);
 
 /*e) Affichez le nom de tous les patients et leur date de naissance.*/
 SELECT pName, pDOB
-FROM medi_schema.patient;
+FROM mediDB.patient;
 
