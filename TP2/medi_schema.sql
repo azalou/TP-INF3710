7b84a3be423ec04250e4cbf706279e9299a8a233
Alassane Maiga <azou8506@gmail.com>
2019-02-13 13:53:21 -0500

    appointments for one patient created

--- TP2/medi_schema.sql
+++ TP2/medi_schema.sql
@@ -10,23 +10,22 @@ CREATE SCHEMA IF NOT EXISTS mediDB;
 SET search_path = mediDB;
 
 CREATE TABLE IF NOT EXISTS mediDB.patient (
-	pID			Varchar(10)		Not NULL,
+	pID			Varchar(10)		UNIQUE Not NULL,
 	pName		Varchar(20)		Not NULL,
-	pNAS        VARCHAR(15)     UNIQUE Not NULL,
-	pAddress	Varchar(100)	Not NULL,
-	pPhone		Varchar(50)		Not NULL,
-	pDOB		Date			Not NULL,
+	pAddress	Varchar(100),
+	pPhone		Varchar(50),
+	pDOB		Date,
 	PRIMARY KEY (pID)
 );
 
 CREATE TABLE IF NOT EXISTS mediDB.doctor(
 	dID			Varchar(10)		UNIQUE Not NULL,
 	dName		Varchar(20)		Not NULL,
-	dAddress	Varchar(100)	Not NULL,
-	dPhone		Varchar(50)		Not NULL,
-	dDOB		Date			Not NULL,
-	dSalary		Numeric(8,2)	Not Null,
-	dMedical    BOOLEAN         Not Null,
+	dAddress	Varchar(100)	,
+	dPhone		Varchar(50)		,
+	dDOB		Date			,
+	dSalary		Numeric(8,2)	,
+	dMedical    BOOLEAN         ,
 	PRIMARY KEY (dID),
 	CONSTRAINT onSalary CHECK (dSalary>=100000.00)
 );
@@ -38,20 +37,21 @@ CREATE TABLE IF NOT EXISTS mediDB.appointment (
 	pID VARCHAR(10) NOT NULL,
 	dID VARCHAR(10) NOT NULL,
 	PRIMARY KEY (aID, pID, dID),
-	FOREIGN KEY (pID) REFERENCES mediDB.patient(pID),
-	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID)
+	FOREIGN KEY (pID) REFERENCES mediDB.patient(pID) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE CASCADE UPDATE CASCADE
+	--CONSTRAINT onDate CHECK (aDate >= current_date)
 );
 
 CREATE TABLE IF NOT EXISTS mediDB.medicald(
 	dID			Varchar(10)		Not NULL,
-	dOtimeRate	Numeric(6,2)	Not NULL,
+	dOtimeRate	Numeric(6,2),
 	PRIMARY KEY (dID),
 	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS mediDB.specialistd(
 	dID			Varchar(10)		Not NULL,
-	dFieldArea	Varchar(30)		Not NULL,
+	dFieldArea	Varchar(30)		,
 	PRIMARY KEY (dID),
 	FOREIGN KEY (dID) REFERENCES mediDB.doctor(dID) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -80,44 +80,48 @@ CREATE TABLE IF NOT EXISTS mediDB.billpayments(
 	FOREIGN KEY (bID) REFERENCES mediDB.bill(bID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE OR REPLACE FUNCTION mediDB.speclialistd_exor_medicald() RETURNS trigger AS $check_mandatoryor$
-BEGIN
--- Check that dID is not in medicald
-IF EXISTS (SELECT dID FROM medidb.medicald WHERE dID = NEW.dID) OR
-	EXISTS (SELECT dID FROM medidb.specialistd WHERE dID = NEW.dID) THEN
-RAISE EXCEPTION 'Doctor cannot be both medicald and specialistd';
-END IF;
-RETURN NEW;
-END;
-$check_mandatoryor$ LANGUAGE plpgsql;
+---- ALTERATIONS
+ALTER TABLE medidb.patient ADD COLUMN pNAS VARCHAR(15) UNIQUE;
 
-CREATE OR REPLACE FUNCTION prevent_delete_on_doctor_with_bill() RETURNS trigger AS
-$check_preventdbil$
-BEGIN
-IF EXISTS (SELECT dID FROM medidb.doctor WHERE dID = OLD.dID) THEN
-    RAISE EXCEPTION 'Cannot delete a doctor that produced a bill';
-END IF;
-RETURN NEW;
-END;
-$check_preventdbil$ LANGUAGE plpgsql;
+
+---- FONCTIONS
+
+-- CREATE OR REPLACE FUNCTION mediDB.speclialistd_exor_medicald() RETURNS trigger AS $check_mandatoryor$
+-- BEGIN
+-- -- Check that dID is not in medicald
+-- IF EXISTS (SELECT dID FROM medidb.medicald WHERE dID = NEW.dID) OR
+-- 	EXISTS (SELECT dID FROM medidb.specialistd WHERE dID = NEW.dID) THEN
+-- RAISE EXCEPTION 'Doctor cannot be both medicald and specialistd';
+-- END IF;
+-- RETURN NEW;
+-- END;
+-- $check_mandatoryor$ LANGUAGE plpgsql;
+
+-- CREATE OR REPLACE FUNCTION prevent_delete_on_doctor_with_bill() RETURNS trigger AS
+-- $check_preventdbil$
+-- BEGIN
+-- IF EXISTS (SELECT dID FROM medidb.doctor WHERE dID = OLD.dID) THEN
+--     RAISE EXCEPTION 'Cannot delete a doctor that produced a bill';
+-- END IF;
+-- RETURN NEW;
+-- END;
+-- $check_preventdbil$ LANGUAGE plpgsql;
     
 
-CREATE TRIGGER exclusive_specialist_medical
-    BEFORE INSERT OR UPDATE 
-    ON medidb.specialistd
-    FOR EACH ROW
-    EXECUTE PROCEDURE medidb.speclialistd_exor_medicald();
-	
-CREATE TRIGGER exclusive_medical_specialist
-    BEFORE INSERT OR UPDATE 
-    ON medidb.medicald
-    FOR EACH ROW
-    EXECUTE PROCEDURE medidb.speclialistd_exor_medicald();
+-- CREATE TRIGGER exclusive_specialist_medical
+--     BEFORE INSERT OR UPDATE 
+--     ON medidb.specialistd
+--     FOR EACH ROW
+--     EXECUTE PROCEDURE medidb.speclialistd_exor_medicald();
+-- 	
+-- CREATE TRIGGER exclusive_medical_specialist
+--     BEFORE INSERT OR UPDATE 
+--     ON medidb.medicald
+--     FOR EACH ROW
+--     EXECUTE PROCEDURE medidb.speclialistd_exor_medicald();
     
-CREATE TRIGGER avoid_delete_if_bill
-    BEFORE DELETE
-    ON medidb.doctor
-    FOR EACH ROW
-    EXECUTE PROCEDURE prevent_delete_on_doctor_with_bill();
-
-
+-- CREATE TRIGGER avoid_delete_if_bill
+--     BEFORE DELETE
+--     ON medidb.doctor
+--     FOR EACH ROW
+--     EXECUTE PROCEDURE prevent_delete_on_doctor_with_bill();
