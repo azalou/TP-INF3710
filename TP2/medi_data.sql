811014926700be288e358535fb771118ff30bea8
Alassane Maiga <azou8506@gmail.com>
2019-02-13 08:55:12 -0500

    other small changes

--- TP2/medi_data.sql
+++ TP2/medi_data.sql
@@ -2,12 +2,10 @@ INSERT into medidb.doctor (dID, dName, dAddress, dPhone, dDOB, dSalary) VALUES (
 INSERT into medidb.specialistd VALUES ('D0001', 'Dermatology');
 INSERT into medidb.doctor VALUES ('D0002', 'Samina Burt', '768 Avenue Hartland, Outremont, QC H2V 2X6', '514-333-43437', DATE'1972-12-23', 130399);
 INSERT into medidb.medicald VALUES ('D0002', 500);
-INSERT into medidb.doctor VALUES ('D0003', 'Arvin Chen', '2541 Duke Street, Montreal, QC H3C 5K4', '514-333-43437', DATE'1972-12-23', 130399);
+INSERT into medidb.doctor VALUES ('D0003', 'Arvin Chen', '2541 Duke Street, Montreal, QC H3C 5K4', '514-333-43437', DATE'1972-12-23', 234399);
 INSERT into medidb.medicald VALUES ('D0003', 600);
 INSERT into medidb.doctor VALUES ('D0004', 'Phoenix Mercado', '2680 rue Levy, Montreal, QC H3C 5K4', '514-111-4437', DATE'1975-10-20', 133229);
 INSERT into medidb.specialistd VALUES ('D0004', 'Immunology');
-
-
 
 
 
