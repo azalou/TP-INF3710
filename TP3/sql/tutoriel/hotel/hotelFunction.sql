4800db51ab67dc351352aa75919f74e0d966ffc2
Alassane Maiga <azou8506@gmail.com>
2019-02-14 09:55:25 -0500

    TP3 start

---
+++ TP3/sql/tutoriel/hotel/hotelFunction.sql
@@ -0,0 +1,15 @@
+﻿SET SEARCH_PATH = hoteldb;
+
+CREATE OR REPLACE FUNCTION totalHotelRecords () RETURNS integer AS $total$
+DECLARE
+	total integer;
+BEGIN
+	SELECT count(*) INTO total FROM hoteldb.hotel;
+	RETURN total;
+END
+$total$ LANGUAGE plpgsql;
+
+select hotelDB.totalHotelRecords();
+
+-- Pour vérifier que la fonction est correcte, comptez le nombre de tuples: 
+select * from Hotel;
---
+++ TP3/sql/tutoriel/triggers/trigger_addressBook.sql
@@ -0,0 +1,42 @@
+﻿DROP TABLE IF EXISTS addressbook CASCADE;
+DROP TABLE IF EXISTS phonebook CASCADE;
+
+CREATE TABLE addressbook (
+id SERIAL Primary Key,
+name VARCHAR(10),
+address VARCHAR (20),
+phoneNum VARCHAR);
+
+
+CREATE TABLE phonebook (
+id SERIAL Primary Key,
+name VARCHAR(10),
+phoneNum VARCHAR);
+
+
+
+CREATE OR REPLACE FUNCTION add_to_phonebook() RETURNS TRIGGER AS $phonebook$
+DECLARE
+	new_name varchar;
+	new_phonenum varchar;
+BEGIN
+	IF (PG_OP='INSERT') THEN
+		INSERT INTO phonebook(name,phonenum) VALUES(NEW.NAME, NEW.phonenum);
+	END IF;
+END
+$phonebook$ LANGUAGE plpgsql;
+
+
+--- Création du trigger
+CREATE TRIGGER phonebook
+AFTER INSERT ON addressbook
+FOR EACH ROW EXECUTE PROCEDURE add_to_phonebook();
+
+
+-- Test du trigger
+insert into addressbook(name, address, phoneNum) VALUES ('AZ','rue des 100 soucis', '613-9696363');
+
+select * from phonebook;
+
+
+
