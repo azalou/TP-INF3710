a8275bd12858dbd00f6d41684efd40cb4ecf8cdd
Alassane Maiga <azou8506@gmail.com>
2019-02-15 21:42:07 -0500

    Added TP5 folder, TP3/universityDB_data.sql, TP3/universityDB_schema.sql

---
+++ TP3/University_data.sql
@@ -0,0 +1,50 @@
+﻿
+
+
+-- DEPARTEMENT
+INSERT into universityDB.dept (dID, dnbrPHDs) VALUES ('genChem', 9);
+INSERT into universityDB.dept (dID, dnbrPHDs) VALUES ('gigl', 40);
+INSERT into universityDB.dept (dID, dnbrPHDs) VALUES ('Maths', 5);
+
+
+-- Cours
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('105', 'programmation','gigl');
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('200', 'NLP','gigl');
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('304',	'Geometrie 101', 'Maths');
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('305',	'Theoremes en geometrie', 'Maths');
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('306',	'Geometrie intermediaire', 'Maths');
+INSERT into universityDB.cours (cID, cName,	dID) VALUES ('307',	'Geometrie','Maths');
+
+
+-- Prof
+INSERT into universityDB.prof (pID, pName, dID) VALUES ('p1', 'AZ', 'gigl');
+INSERT into universityDB.prof (pID, pName, dID) VALUES ('p2', 'MG', 'gigl');
+INSERT into universityDB.prof (pID, pName, dID) VALUES ('p3', 'NZ', 'Maths');
+INSERT into universityDB.prof (pID, pName, dID) VALUES ('p4', 'LH', 'Maths');
+
+
+-- Section
+INSERT into universityDB.section (cID, secID, pID) VALUES ('105', '1', 'p1');
+INSERT into universityDB.section (cID, secID, pID) VALUES ('105', '2', 'p1');	
+INSERT into universityDB.section (cID, secID, pID) VALUES ('305', '1', 'p3');
+INSERT into universityDB.section (cID, secID, pID) VALUES ('305', '2', 'p4');
+
+
+--ETUDIANTS
+
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Simon Nissan', 's1', 'M', 20, '4.2');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Laurent Passepartout', 's2', 'M', 30, '3.2');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Alexandra Laplace', 's3', 'F', 40, '2.8');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Alex Laplace', 's4', 'F', 30, '3.5');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Simon Belanger', 's5', 'M', 20, '3.2');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Mark Zuck', 's6', 'M', 30, '1.2');
+INSERT into universityDB.etudiant (sName, sID, sSex, sAge, sGPA) VALUES ('Sophie Yenamarre', 's7', 'M', 30, '4.2');
+
+
+-- INSCRIPTION
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s3', '105', '1', 90);
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s2', '105', '1', 60);
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s1', '105', '2', 70);
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s5', '105', '2', 70);
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s5', '305', '2', 100);
+INSERT into universityDB.inscription (sID, cID, secID, note) VALUES ('s6', '305', '2', 65);
---
+++ TP3/University_schema.sql
@@ -0,0 +1,60 @@
+﻿DROP SCHEMA IF EXISTS universityDB CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS universityDB;
+
+SET search_path = universityDB;
+
+CREATE DOMAIN universityDB.SEX AS CHAR
+	CHECK (VALUE IN ('M', 'F'));
+
+CREATE TABLE IF NOT EXISTS universityDB.etudiant (
+	sID		Varchar(10)		Not NULL,
+	sName      	Varchar(50)		Not NULL,
+	sSex		SEX,
+	sAge		INTEGER,
+	sGPA		NUMERIC(3,2),
+	PRIMARY KEY (sID)
+);
+
+CREATE TABLE IF NOT EXISTS universityDB.dept(
+	dID		Varchar(10)       Not NULL,
+	dnbrPHDs	INTEGER          Not NULL,
+	PRIMARY KEY (dID)
+);
+
+CREATE TABLE IF NOT EXISTS universityDB.prof(
+	pID		Varchar(10)       Not NULL,
+	pName		VARCHAR(50)       Not NULL,
+	dID	        Varchar(10)        Not NULL,
+	PRIMARY KEY (pID),
+	FOREIGN KEY (dID) REFERENCES dept(dID)
+);
+
+CREATE TABLE IF NOT EXISTS universityDB.cours(
+	cID		Varchar(10)       Not NULL,
+	cName		VARCHAR(50)       Not NULL,
+	dID	        Varchar(10)        Not NULL,
+	PRIMARY KEY (cID),
+	FOREIGN KEY (dID) REFERENCES dept(dID)
+);
+
+CREATE TABLE IF NOT EXISTS universityDB.section(
+	cID		Varchar(10)       Not NULL,
+	secID		VARCHAR(10)       Not NULL,
+	pID		Varchar(10)        Not NULL,
+	PRIMARY KEY (cID, secID),
+	FOREIGN KEY (pID) REFERENCES prof(pID) ON DELETE SET NULL,
+	FOREIGN KEY (cID) REFERENCES cours(cID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS universityDB.inscription(
+    sID         Varchar(10)         Not NULL,
+	cID			Varchar(10)         Not NULL,
+	secID	    VARCHAR(10)         Not NULL,
+	note        INTEGER        Not NULL,
+	PRIMARY KEY (sID, cID, secID),
+	FOREIGN KEY (sID) REFERENCES etudiant(sID),
+	FOREIGN KEY (cID, secID) REFERENCES section(cID, secID)
+);
+
+
