465f99cb2c85530714cac8db2fba3192dccc1f51
Alassane Maiga <azou8506@gmail.com>
2019-03-22 16:20:30 -0400

    schema done

--- TP5/Schema_Veto_sans_front.sql
+++ TP5/Schema_Veto_sans_front.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA IF EXISTS vetoSansFrontiere CASCADE;
+﻿DROP SCHEMA IF EXISTS vetoSansFrontiere CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS vetoSansFrontiere;
 
@@ -10,8 +10,8 @@ CREATE DOMAIN SEX AS CHAR
 CREATE DOMAIN DOA AS VARCHAR(5)
     CHECK (VALUE IN ('Dead','Alive'));
 
-CREATE TABLE IF NOT EXISTS Clinique (
-    cID         Varchar(10)		Not NULL,
+CREATE TABLE IF NOT EXISTS Clinic (
+    cID         Varchar(10)		UNIQUE Not NULL,
     phone       Varchar(20),
     fax         Varchar(50),
     road        Varchar(50),
@@ -21,72 +21,78 @@ CREATE TABLE IF NOT EXISTS Clinique (
     PRIMARY KEY (cID)
 );
 
-CREATE TABLE IF NOT EXISTS Personel (
-    persID      Varchar(10)     Not NULL,
+CREATE TABLE IF NOT EXISTS Employe (
+    eID         Varchar(10)     Not NULL,
+    cID         Varchar(10)		Not NULL,
     NAS         Varchar(50)     Unique Not NULL,
-    name        Varchar(50),
+    Ename       Varchar(50),
     surname     Varchar(50),
     phone       Varchar(20),
     DOB         Date,
-    sex         SEX,
-    position    Varchar(50),
-    salary      Numeric(7,2)
+    Esex         SEX,
+    Eposition    Varchar(50),
+    salary      Numeric(7,2),
     road        Varchar(50),
     city        Varchar(50),
     province    Varchar(10),
     pCode       Varchar(10),
-    PRIMARY KEY (persID, name),
-    ALTERNATE KEY (NAS)
+    PRIMARY KEY (eID),
+    FOREIGN KEY (cID) REFERENCES Clinic(cID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Owner (
     ownerID     Varchar(10)		Not NULL,
-    name        Varchar(50),
+    cID         Varchar(10)		Not NULL,
+    Oname       Varchar(50),
     surname     Varchar(50),
     phone       Varchar(20),
     road        Varchar(50),
     city        Varchar(50),
     province    Varchar(10),
     pCode       Varchar(10),
-    PRIMARY KEY (ownerID)
+    UNIQUE (ownerID,cID),
+    PRIMARY KEY (ownerID, cID),
+    FOREIGN KEY (cID) REFERENCES Clinic(cID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Pet (
     ownerID     Varchar(10)		Not NULL,
-    petID       Varchar(10)     Not NULL,
+    cID         Varchar(10)		Not NULL,
+    petID       NUMERIC(2,0)     	Not NULL,
     specie      Varchar(20)     Not NULL,
     description Varchar,
     DOB         Date,
     status      DOA,
-    PRIMARY KEY (ownerID, petID),
-    FOREIGN KEY (ownerID) REFERENCES Owner(ownerID) ON DELETE RESTRICT ON UPDATE CASCADE
+    UNIQUE (ownerID, cID, petID),
+    PRIMARY KEY (ownerID, cID, petID),
+    FOREIGN KEY (ownerID, cID) REFERENCES Owner(ownerID, cID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Enrollment (
-    cID         Varchar(10)		Not NULL,
-    petID       Varchar(10)     Not NULL,
+    enID	Varchar(10)		Not NULL,
     ownerID     Varchar(10)		Not NULL,
-    enrol_date  Date            Not NULL,
-    PRIMARY KEY (cID, petID, ownerID),
-    FOREIGN KEY (cID) REFERENCES Clinique(cID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (petID) REFERENCES Pet(petID) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (ownerID) REFERENCES Owner(ownerID) ON DELETE ON DELETE RESTRICT ON UPDATE CASCADE
+    cID         Varchar(10)		Not NULL,
+    petID       NUMERIC(2,0)     	Not NULL,
+    enrol_date  Date            	Not NULL,
+    PRIMARY KEY (enID),
+    FOREIGN KEY (ownerID, cID, petID) REFERENCES Pet(ownerID, cID, petID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Physical_Exam (
-    persID      Varchar(10)     Not NULL,
-    vetName     Varchar(50)     Not NULL,
-    petID       Varchar(10)     Not NULL,
+    eID         Varchar(10)     Not NULL,
+    ownerID     Varchar(10)		Not NULL,
+    cID         Varchar(10)		Not NULL,
+    petID       NUMERIC(2,0)     	Not NULL,
     examID      Varchar(10)     Not NULL,
     examDate    Date            Not NULL,
     examTime    Time            Not NULL,
     description VARCHAR,
-    PRIMARY KEY (persID, vetName, petID, examID, examDate),
-    FOREIGN KEY (persID, vetName) REFERENCES Personel(persID, name),
-    FOREIGN KEY (petID) REFERENCES Pet(petID) ON DELETE RESTRICT ON UPDATE CASCADE
+    PRIMARY KEY (examID),
+    FOREIGN KEY (eID) REFERENCES Employe(eID) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (ownerID, cID, petID) REFERENCES Pet(ownerID, cID, petID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS treatment (
+CREATE TABLE IF NOT EXISTS Treatment (
     treatID     Varchar(10)     Not NULL,
     description Varchar,
     tcost       Numeric(5,2),
@@ -96,13 +102,14 @@ CREATE TABLE IF NOT EXISTS treatment (
 CREATE TABLE IF NOT EXISTS ProposedTreatment(
     treatID     Varchar(10)     Not NULL,
     examID      Varchar(10)     Not NULL,
-    examDate    Date            Not NULL,
-    petID       Varchar(10)     Not NULL,
+    ownerID     Varchar(10)		Not NULL,
+    cID         Varchar(10)		Not NULL,
+    petID       NUMERIC(2,0)     	Not NULL,
     qtity       INTEGER         Not NULL,
     sDate       Date,
     eDate       Date,
-    PRIMARY KEY (treatID, examID, examDate, petID),
-    FOREIGN KEY (examID, examDate) REFERENCES Physical_Exam(examID, examDate) ON DELETE RESTRICT ON UPDATE CASCADE
-    FOREIGN KEY (petID) REFERENCES Pet(petID) ON DELETE RESTRICT ON UPDATE CASCADE
+    PRIMARY KEY (treatID),
+    FOREIGN KEY (examID) REFERENCES Physical_Exam(examID) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (ownerID, cID, petID) REFERENCES Pet(ownerID, cID, petID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
