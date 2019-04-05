5ad34e25027ba56dc95167b51711dbb947281112
Alassane Maiga <azou8506@gmail.com>
2019-04-04 22:28:25 -0400

    Web interface debut

--- TP5/node-api-vetosf/index.js
+++ TP5/node-api-vetosf/index.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const db = require('./queries')
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+const app = express();
+const mnt = require('moment');
+const path = require('path');
+const logger = (req, resp, next) => {
+	console.log(`${mnt().format()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+	next();
+};
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+//init middleware
+app.use(logger);
 
+// set static folder
+app.use(express.static(path.join(__dirname, 'VetoSF')));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'VetoSF', 'VetoSF.html'))
+});
+
+// get list of members
+app.get('/members', db.getOwners);
+app.get('/members/:id', db.getOwnerByID)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
--- TP5/node-api-vetosf/queries.js
+++ TP5/node-api-vetosf/queries.js
@@ -1,11 +1,30 @@
 const Pool = require('pg').Pool
-
-
 const pool = new Pool({
-  user: 'azalou',
-  host: 'localhost',
-  database: 'TP5',
-  password: 'inf3710',
-  port: 5432,
+	user: 'azalou',
+	host: 'localhost',
+    database: 'vetosf',
+	password: '12345',
+	port: 5432,
 })
 
+const getOwners = (request, response) => {
+    pool.query('SELECT * FROM vetosansfrontiere.owner ORDER BY cid ASC', (error, results) => {
+        if (error) {
+        throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+const getOwnerByID = (request, response) => {
+    const id = request.params.id
+    pool.query('SELECT * FROM vetosansfrontiere.owner WHERE ownerID = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+            }
+            response.status(200).json(results.rows)
+    })
+}
+module.exports = {
+    getOwners,
+    getOwnerByID,
+  }
